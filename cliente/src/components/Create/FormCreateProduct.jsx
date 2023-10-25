6e0b5babbaa83f1397af7f3bfeba201c24802e5d
AveMat81 <matiasavesani@yahoo.com.ar>
2023-10-25 16:33:13 -0300

    feat: inicio create products

---
+++ cliente/src/components/Create/FormCreateProduct.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function validate(input) {
+
+}
+
+
+export default function CreateProduct() {
+    const dispatch = useDispatch();
+    
+}
