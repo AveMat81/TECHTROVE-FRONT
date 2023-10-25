ef9c338ebfbc37d58836a0502612befd77e9fd7b
AveMat81 <matiasavesani@yahoo.com.ar>
2023-10-25 15:27:52 -0300

    inicio routes y route

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import Home from './views/Home';
+import Detail from './views/Detail';
+import './App.css';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
  
 
   return (
     <div>
-
+      <NavBar/>
+      <Routes>
+        <Route path= "/" element={<Home/>}/>
+        <Route path= "/detail/:id" element={<Detail/>}/>
+      </Routes>
     </div>
   )
 }
