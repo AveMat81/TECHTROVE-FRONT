d00794d004bed4917a8c86dc2410af4c27ad645e
AveMat81 <matiasavesani@yahoo.com.ar>
2023-10-26 18:18:10 -0300

    configuracion tailwind

--- cliente/tailwind.config.js
+++ cliente/tailwind.config.js
@@ -1,25 +1,11 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Archivos de JavaScript y JSX en tu proyecto
-    "./public/index.html", // Archivo HTML principal
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    // screens: {
-    //   'tablet': '640px',
-    //   // => @media (min-width: 640px) { ... }
-
-    //   'laptop': '1024px',
-    //   // => @media (min-width: 1024px) { ... }
-
-    //   'desktop': '1280px',
-    //   // => @media (min-width: 1280px) { ... }
-    // }
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
