a4454c54fd6f887bd10cd54ef55653c83c1aef3b
Esteban Yguia <estebanyguia16@gmail.com>
2023-11-16 01:21:49 -0500

    respaldo

--- src/components/SimpleRating.jsx
+++ src/components/SimpleRating.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { FaStar } from "react-icons/fa"
 
 export default function SimpleRating({ setRaitingBack}) {
   const [value, setValue] = React.useState(0);
@@ -31,6 +32,8 @@ export default function SimpleRating({ setRaitingBack}) {
           // }}
         />
       </Box>
+      <FaStar />
+
       {/* <Box component="fieldset" mb={3} borderColor="transparent">
         <Typography component="legend">Read only</Typography>
         <Rating name="read-only" value={value} readOnly />
